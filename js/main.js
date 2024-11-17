7f2dd36076011a4bf96237623fc443413b66a1a3
perdreauarthur <arthur.perdreau@outlook.fr>
2024-11-18 00:35:10 +0100

    le jeu fonctionne

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
-//fonction permettant de récupérer la lettre saisi par le joueur
-function recuperationLettre() {
-    let lettre = document.getElementById("input").value;
-    //alert(lettre);
-    return lettre;
-}
-
-let alphabet="abcdefghijklmnopqrstuvwxyz";
 
 //liste de mots parmis laquelle l'ordinateur choisira le mot à trouver
+const valider = document.querySelector('.buttonValider');
+const input = document.querySelector('.saisiLettre');
+const motEnProgression = document.querySelector('.motEnCours');
+const message=document.querySelector('.messageJeu');
+const nbVies=document.getElementById('nombreVie');
+const lettresDejaUtilisees= document.querySelector('.lettresDejaUtilises');
 const listeMots = ['dragon', 'flemme', 'bleu', 'coder', 'manger', 'jouer', 'dormir', 'monstre', 'console', 'boire', 'baleine', 'chat', 'dauphin', 'huile', 'angle', 'armoire', 'banc', 'bureau', 'cabinet', 'carreau', 'chaise', 'classe', 'coin', 'couloir', 'dossier'];
+let vies=7;
+let motAtrouver= choixMotADeviner()
+let motTirets=creationMotTiret(motAtrouver)
+let lettresUtilisees=[];
+revelationPremiereEtDeuxiemeLettre()
+valider.addEventListener("click",lancementJeu)
+
+
+
+
+
+
+//fonction permettant de récupérer la lettre saisi par le joueur
+function recuperationLettre() {
+    let lettre = document.querySelector(".saisiLettre").value;
+    //alert(lettre);
+    return lettre.toLowerCase();
+}
+
 
 //fonction permettant de choisir le mot à deviner parmis l'array listMots
 function choixMotADeviner() {
@@ -17,7 +34,6 @@ function choixMotADeviner() {
     let motADeviner = listeMots[nbArrondi];
     return motADeviner;
 }
-
 //fonction permettant d'afficher le mot sous forme de tirets
 function creationMotTiret(motADeviner) {
     let motTiret= motADeviner.split('');
@@ -26,55 +42,50 @@ function creationMotTiret(motADeviner) {
     }
     return motTiret;
 }
-function motTrouve(motEncours){
-    for (let i = 0; i < motEncours.length; i++) {
-        if (motEncours[i]==="_"){
-            return true;
-        }
-    }
-
-}
-function lancerPartie(){
-    let motFinalADeviner=choixMotADeviner(); // --> ex: bateau
-    let vieDuJoueur=5; //--> initialisation du nombre de vie
-    let lettreSaisieParJoueur; //--> initialisation de la variable lettreSaisieParJoueur à none
-    let motTiret=creationMotTiret(motFinalADeviner); // --> ["_","_","_"]
-    let nbLettreTrouve=0;
-    while (vieDuJoueur > 0 && motTrouve){ //--> tant que le joueur a encore des vies et qu'il n'a pas trouvé le mot
-        lettreSaisieParJoueur=recuperationLettre();//--> actualisation de la variable lettreSaisieParJoueur avec une lettre
-        for (let i = 0; i < motFinalADeviner.length; i ++) {
-            if (lettreSaisieParJoueur.toLowerCase() === motFinalADeviner[i]) {
-                motTiret[i] = lettreSaisieParJoueur.toLowerCase();
-                nbLettreTrouve++;
+function revelationPremiereEtDeuxiemeLettre() {
+    let premiereEtDeuxiemeLettre=[motAtrouver[0],motAtrouver[motAtrouver.length-1]];
+    for (let i = 0; i < 2; i ++) {
+        for (let j = 0; j < motAtrouver.length; j ++) {
+            if (premiereEtDeuxiemeLettre[i] ===motAtrouver[j]) {
+                motTirets[j]=premiereEtDeuxiemeLettre[i]
             }
         }
-        if(nbLettreTrouve ===0) {
-            vieDuJoueur --; //--> décrémenter les vies si pas de lettre trouvée
-            console.log(vieDuJoueur);
-        }
-        else {
-            nbLettreTrouve = 0; //--> reset du nombre de lettre trouvée pour le prochain tour
-            console.log(nbLettreTrouve);
-        }
-        console.log(motTiret);
-    }
-    if (vieDuJoueur === 0) {
-        alert("Vous avez perdu !")
-            }else{
-        alert("Bravo tu a trouvé le mot")
     }
 }
-lancerPartie();
+lettresDejaUtilisees.innerHTML=`<p>Lettres utilisées : ${lettresUtilisees}</p>`;
+motEnProgression.innerHTML=motTirets;
+function lancementJeu(){
+    const lettreDonnee=recuperationLettre();
+    input.value="";
+    if (lettresUtilisees.includes(lettreDonnee)){// in ne fonctionne pas
+        message.innerHTML = `<p>Lettre déjà utilisée !</p>`;
+    }else{
+        lettresUtilisees.push(lettreDonnee)
+        lettresDejaUtilisees.innerHTML=`<p>Lettres utilisées : ${lettresUtilisees}</p>`;
+    }
+
+    if (motAtrouver.includes(lettreDonnee )) {
+        for (let k = 0; k < motAtrouver.length; k++) {
+            if (lettreDonnee === motAtrouver[k]) {
+                motTirets[k] = motAtrouver[k];
+            }
+        }
+        motEnProgression.innerHTML=motTirets;
+    }else {
+            vies-=1;
+            nbVies.innerHTML=`Nombre de vie : ${vies}`
+            message.innerHTML = `<p>Lettre pas comprise dans le mot !</p>`;}
 
 
-//const valider = document.querySelector('.buttonValider');
-//let mot=choixMotADeviner();
-//let motTiret= creationMotTiret(mot);
-//let motAtrouver=motTiret;
-//let lettreSaisie=recuperationLettre();
+    if(motTirets.includes("_" )) {
+    }else{
+        alert(`Vous avez gagné. Le mot était ${motAtrouver}.`)
+    }
+
+    if(vies===0){
+        alert(`Vous avez perdu. Le mot à trouver était ${motAtrouver}.`)
+    }
+}
 
 
-//valider.addEventListener("click", remplacementTiret);
-//console.log(mot);
-//console.log(motAtrouver);
 
